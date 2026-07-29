5e3a74693d7697a5717458f18a9e86c503ffb36e
sschwindt <sebastian.schwindt@alumni.epfl.ch>
2026-07-29 13:50:24 +0200

    cleaned up

--- program-1.4/Tools/run_make_annual_flow_duration.bat
+++
@@ -1,5 +0,0 @@
-@echo Make flow duration curve
-
-call "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\propy" "%cd%\make_annual_flow_duration.py"
-exit
-
--- program-1.4/Tools/run_make_mu.bat
+++
@@ -1,5 +0,0 @@
-@echo Make Morphological Units raster
-
-call "%PROGRAMFILES%\ArcGIS\Pro\bin\Python\Scripts\propy" "%cd%\make_mu.py"
-exit
-
